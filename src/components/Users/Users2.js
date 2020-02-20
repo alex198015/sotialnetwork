41ee7067198b3579e6c0af6e7f7c86877b777cf6
vasy <kitten20811@gmail.com>
2020-02-20 20:33:08 +0200

    lesson 54

--- src/components/Users/Users2.js
+++ src/components/Users/Users2.js
@@ -8,23 +8,15 @@ import userPhoto from './../../assets/images/user.jpg'
 
 class Users extends React.Component {
     constructor(props) {
-        super(props)
+        super(props)   
+    }
 
+    componentDidMount(){
         axios.get("https://social-network.samuraijs.com/api/1.0/users")
             .then(response => {
                 this.props.onSetUsers(response.data.items)
             })
     }
-
-    // getUsers = () => {
-    //     if (this.props.users.length === 0) {
-    //         axios.get("https://social-network.samuraijs.com/api/1.0/users")
-    //             .then(response => {
-    //                 this.props.onSetUsers(response.data.items)
-    //             })
-
-    //     }
-
 
 
     render() {
