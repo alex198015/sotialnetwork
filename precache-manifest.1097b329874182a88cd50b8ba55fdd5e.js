923160451f20e6d0c9acb89b593a413e44c054d3
vasy <kitten20811@gmail.com>
2020-03-11 20:19:21 +0200

    Updates

---
+++ precache-manifest.1097b329874182a88cd50b8ba55fdd5e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78c186342ed555740434528fbbaff021",
+    "url": "/sotialnetwork/index.html"
+  },
+  {
+    "revision": "5685952fcb230c9e9ceb",
+    "url": "/sotialnetwork/static/css/2.0996cbbd.chunk.css"
+  },
+  {
+    "revision": "824c31a2f175f935a7e4",
+    "url": "/sotialnetwork/static/css/3.98039f08.chunk.css"
+  },
+  {
+    "revision": "8bbc82ddfc7af41b2870",
+    "url": "/sotialnetwork/static/css/4.be96ada2.chunk.css"
+  },
+  {
+    "revision": "32881afdbfb9fff29a7c",
+    "url": "/sotialnetwork/static/css/main.c4a09bf5.chunk.css"
+  },
+  {
+    "revision": "5685952fcb230c9e9ceb",
+    "url": "/sotialnetwork/static/js/2.99197e4c.chunk.js"
+  },
+  {
+    "revision": "33c7420501d7b0349de46a3a627e2c05",
+    "url": "/sotialnetwork/static/js/2.99197e4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "824c31a2f175f935a7e4",
+    "url": "/sotialnetwork/static/js/3.1dc1aeb9.chunk.js"
+  },
+  {
+    "revision": "8bbc82ddfc7af41b2870",
+    "url": "/sotialnetwork/static/js/4.08f4d2f2.chunk.js"
+  },
+  {
+    "revision": "32881afdbfb9fff29a7c",
+    "url": "/sotialnetwork/static/js/main.a86f5a1d.chunk.js"
+  },
+  {
+    "revision": "3b84eedadff02c1a0559",
+    "url": "/sotialnetwork/static/js/runtime-main.79f6301a.js"
+  },
+  {
+    "revision": "5dc7e10bc57cadc681642e50f6b8daa4",
+    "url": "/sotialnetwork/static/media/preloader.5dc7e10b.svg"
+  },
+  {
+    "revision": "a5086149d635820e50b80d8f6a630c51",
+    "url": "/sotialnetwork/static/media/user.a5086149.jpg"
+  }
+]);
